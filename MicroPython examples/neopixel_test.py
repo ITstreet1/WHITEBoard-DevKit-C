5a246ec8511bfb3b21e0ccc56d145a2d478abece
Dejan Petrović <dekip@beotel.rs>
2022-04-27 21:17:39 +0200

    Update neopixel_test.py

--- MicroPython examples/neopixel_test.py
+++ MicroPython examples/neopixel_test.py
@@ -2,7 +2,7 @@ from machine import Pin
 from neopixel import NeoPixel
 import time
 
-pin = Pin(8, Pin.OUT)   # set GPIO0 to output to drive NeoPixels
+pin = Pin(8, Pin.OUT)   # set GPIO to output to drive NeoPixels
 np = NeoPixel(pin, 1)   # create NeoPixel driver on GPIO for n pixels
 
 while True:
